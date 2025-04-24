ecf3336a25b0706155cd397f945366f9bd4d8468
parvez <parvezreza112@gmail.com>
2025-04-24 13:34:57 +0600

    added footer section

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,7 +1,80 @@
 import React from "react";
+import bannerImg from "../assets/logo.png";
+import { Link, NavLink } from "react-router";
+import { FaFacebook, FaLinkedin, FaTwitter, FaYoutube } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
 
 const Footer = () => {
-  return <div>this is footer</div>;
+  return (
+    <>
+      <footer className="footer footer-horizontal footer-center bg-[#0F0F0F]  text-white rounded p-10">
+        <nav>
+          <div className="grid grid-flow-col gap-4">
+            <Link to={"/"} className=" text-xl">
+              <div className="flex gap-1 items-center ">
+                <img src={bannerImg} alt="" />
+                <h1 className="text-4xl font-extrabold">Law.BD</h1>
+              </div>
+            </Link>
+          </div>
+        </nav>
+        <nav className="grid grid-flow-col gap-4">
+          <ul className="menu menu-horizontal px-1 text-lg font-medium text-[#FFFFFF70]">
+            <li>
+              <NavLink
+                to={"/"}
+                className={({ isActive }) => (isActive ? "underline" : "")}
+              >
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to={"/my-bookings"}
+                className={({ isActive }) => (isActive ? "underline" : "")}
+              >
+                My-Bookings
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to={"/blogs"}
+                className={({ isActive }) => (isActive ? "underline" : "")}
+              >
+                Blogs
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to={"/contact-us"}
+                className={({ isActive }) => (isActive ? "underline" : "")}
+              >
+                Contact Us
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+        <div className="border w-full border-dashed border-[#FFFFFF70]"></div>
+        <nav>
+          <div className="grid grid-flow-col gap-4">
+            <a>
+              <FaFacebook size={50} className=" p-2" />
+            </a>
+
+            <a>
+              <FaXTwitter size={50} className="p-2"></FaXTwitter>
+            </a>
+            <a>
+              <FaLinkedin size={50} className="p-2"></FaLinkedin>
+            </a>
+            <a>
+              <FaYoutube size={50} className=" p-2"></FaYoutube>
+            </a>
+          </div>
+        </nav>
+      </footer>
+    </>
+  );
 };
 
 export default Footer;
--- src/Roots/Roots.jsx
+++ src/Roots/Roots.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 import { Outlet } from "react-router";
 import Navbar from "../Components/Navbar";
+import Footer from "../Components/Footer";
 
 const Roots = () => {
   return (
     <>
       <Navbar></Navbar>
-      <div>
+      <div className="min-h-[calc(100vh-288px)]">
         <div className="max-w-screen-2xl mx-auto px-8 md:px-12 lg:px-24">
           <Outlet></Outlet>
         </div>
       </div>
+      <Footer></Footer>
     </>
   );
 };
