21b99521ba85f7b208f9e7c841f30258d2eadf7e
parvez <parvezreza112@gmail.com>
2025-04-24 23:42:37 +0600

    added error massage

--- src/Pages/ErrorPage.jsx
+++ src/Pages/ErrorPage.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import Navbar from "../Components/Navbar";
+import { Link, useRouteError } from "react-router";
 
 const ErrorPage = () => {
+  const error = useRouteError();
+  console.log(error);
   return (
     <div>
       <Navbar></Navbar>
-      <h1>NOt found</h1>
+      <div className="py-24 text-center">
+        <h1>{error?.status || 404}</h1>
+        <p>{error?.error?.message || "No Page Found"}</p>
+        <Link to={"/"}>
+          <button className="btn btn-success">GO TO HOME</button>
+        </Link>
+      </div>
     </div>
   );
 };
